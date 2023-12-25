fa682add144353907b2037d98ef2496b16d22f4d
srimant03 <107833913+srimant03@users.noreply.github.com>
2023-12-26 01:14:59 +0530

    Add files via upload

---
+++ custom_topology.py
@@ -0,0 +1,30 @@
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    def build(self):
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
+        h6 = self.addHost('h6')
+        h7 = self.addHost('h7')
+        h8 = self.addHost('h8')
+
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s2)
+        self.addLink(h4, s2)
+        self.addLink(h5, s2)
+        self.addLink(h6, s3)
+        self.addLink(h7, s3)
+        self.addLink(h8, s3)
+
+        self.addLink(s1, s2)
+        self.addLink(s2, s3)
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
